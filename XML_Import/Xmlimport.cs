d4f13c1a3c48eaaf4510efc79ec6596a6e4ed5f7
mini_ <mini_@DESKTOP-946TJP2>
2017-04-25 12:53:33 +0200

    hest

--- XML_Import/Xmlimport.cs
+++ XML_Import/Xmlimport.cs
@@ -25,155 +25,155 @@ namespace XML_Import
         public string linkUrl { get; set; }
 
 
-            Mobile
-            Phone
-            Created
-            CreatedBy
-            DescriptionType
-            DescriptionTypeID
-            Html
-            Text
-            EksternalLinkName
-            EksternalUrl
-            EksternalLinkType
+//            Mobile
+//            Phone
+//            Created
+//            CreatedBy
+//            DescriptionType
+//            DescriptionTypeID
+//            Html
+//            Text
+//            EksternalLinkName
+//            EksternalUrl
+//            EksternalLinkType
 
 
+//            asd
+
+//            File1Alttext
+//            File1Copyright
+//            File1Description
+//            file1Filetype
+//            File1ID
+//            File1Metatag
+//            File1Name
+//            File1Photographer
+//            File1Uri
+
+//            File2Alttext
+//            File2Copyright
+//            File2Description
+//            file2Filetype
+//            File2ID
+//            File2Metatag
+//            File2Name
+//            File2Photographer
+//            File2Uri
+
+//                        File3Alttext
+//            File3Copyright
+//            File3Description
+//            file3Filetype
+//            File3ID
+//            File3Metatag
+//            File3Name
+//            File3Photographer
+//            File3Uri
+
+//                        File4Alttext
+//            File4Copyright
+//            File4Description
+//            file4Filetype
+//            File4ID
+//            File4Metatag
+//            File4Name
+//            File4Photographer
+//            File4Uri
+
+//                        File5Alttext
+//            File5Copyright
+//            File5Description
+//            file5Filetype
+//            File5ID
+//            File5Metatag
+//            File5Name
+//            File5Photographer
+//            File5Uri
+
+//                        File6Alttext
+//            File6Copyright
+//            File6Description
+//            file6Filetype
+//            File6ID
+//            File6Metatag
+//            File6Name
+//            File6Photographer
+//            File6Uri
+
+//            MaincatID
+//            MaincatName
+
+//            Channelname1
+//            ChannelID1
+
+//            Channelname2
+//            ChannelID2
+
+//            Channelname3
+//            ChannelID3
+
+//Channelname4
+//            ChannelID4
 
 
-            File1Alttext
-            File1Copyright
-            File1Description
-            file1Filetype
-            File1ID
-            File1Metatag
-            File1Name
-            File1Photographer
-            File1Uri
-
-            File2Alttext
-            File2Copyright
-            File2Description
-            file2Filetype
-            File2ID
-            File2Metatag
-            File2Name
-            File2Photographer
-            File2Uri
-
-                        File3Alttext
-            File3Copyright
-            File3Description
-            file3Filetype
-            File3ID
-            File3Metatag
-            File3Name
-            File3Photographer
-            File3Uri
-
-                        File4Alttext
-            File4Copyright
-            File4Description
-            file4Filetype
-            File4ID
-            File4Metatag
-            File4Name
-            File4Photographer
-            File4Uri
-
-                        File5Alttext
-            File5Copyright
-            File5Description
-            file5Filetype
-            File5ID
-            File5Metatag
-            File5Name
-            File5Photographer
-            File5Uri
-
-                        File6Alttext
-            File6Copyright
-            File6Description
-            file6Filetype
-            File6ID
-            File6Metatag
-            File6Name
-            File6Photographer
-            File6Uri
-
-            MaincatID
-            MaincatName
-
-            Channelname1
-            ChannelID1
-
-            Channelname2
-            ChannelID2
-
-            Channelname3
-            ChannelID3
-
-Channelname4
-            ChannelID4
+//            Channelname5
+//            ChannelID5
 
 
-            Channelname5
-            ChannelID5
+//            Channelname6
+//            ChannelID6
 
 
-            Channelname6
-            ChannelID6
+//            Channelname7
+//            ChannelID7
 
 
-            Channelname7
-            ChannelID7
+//            Channelname8
+//            ChannelID8
 
 
-            Channelname8
-            ChannelID8
+//            Channelname9
+//            ChannelID9
 
 
-            Channelname9
-            ChannelID9
+//            Channelname10
+//            ChannelID10
 
 
-            Channelname10
-            ChannelID10
+//            Channelname11
+//            ChannelID11
 
 
-            Channelname11
-            ChannelID11
+//            Channelname12
+//            ChannelID12
 
 
-            Channelname12
-            ChannelID12
+//            Channelname13
+//            ChannelID13
 
 
-            Channelname13
-            ChannelID13
+//            Channelname14
+//            ChannelID14
 
 
-            Channelname14
-            ChannelID14
+//            Channelname15
+//            ChannelID15
 
 
-            Channelname15
-            ChannelID15
+//            Channelname16
+//            ChannelID16
 
 
-            Channelname16
-            ChannelID16
+//            Channelname17
+//            ChannelID17
 
 
-            Channelname17
-            ChannelID17
+//            Channelname18
+//            ChannelID18
 
 
-            Channelname18
-            ChannelID18
-
-
-            Channelname19
-            ChannelID19
+//            Channelname19
+//            ChannelID19
 
 
 
