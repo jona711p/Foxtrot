eef99ec3ac9ef08825bfc9787a6922b139e921c7
mini_ <mini_@DESKTOP-946TJP2>
2017-04-25 12:45:32 +0200

    .

--- XML_Import/Xmlimport.cs
+++ XML_Import/Xmlimport.cs
@@ -8,12 +8,175 @@ namespace XML_Import
 {
     class Xmlimport
     {
-        private string adress;
+        public string Adress { get; set; }
+        public string City { get; set; }
+        public string Latitude { get; set; }
+        public string Longitude { get; set; }
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string PostalCode { get; set; }
+        public string Region { get; set; }
+        public string CanonicalUrl { get; set; }
+        public string catId { get; set; }
+        public string catName { get; set; }
+        public string Email { get; set; }
+        public string Fax { get; set; }
+        public string linkName { get; set; }
+        public string linkUrl { get; set; }
 
-        public string Adress
-        {
-            get { return adress; }
-            set { adress = value; }
-        }
+
+            Mobile
+            Phone
+            Created
+            CreatedBy
+            DescriptionType
+            DescriptionTypeID
+            Html
+            Text
+            EksternalLinkName
+            EksternalUrl
+            EksternalLinkType
+
+
+
+
+            File1Alttext
+            File1Copyright
+            File1Description
+            file1Filetype
+            File1ID
+            File1Metatag
+            File1Name
+            File1Photographer
+            File1Uri
+
+            File2Alttext
+            File2Copyright
+            File2Description
+            file2Filetype
+            File2ID
+            File2Metatag
+            File2Name
+            File2Photographer
+            File2Uri
+
+                        File3Alttext
+            File3Copyright
+            File3Description
+            file3Filetype
+            File3ID
+            File3Metatag
+            File3Name
+            File3Photographer
+            File3Uri
+
+                        File4Alttext
+            File4Copyright
+            File4Description
+            file4Filetype
+            File4ID
+            File4Metatag
+            File4Name
+            File4Photographer
+            File4Uri
+
+                        File5Alttext
+            File5Copyright
+            File5Description
+            file5Filetype
+            File5ID
+            File5Metatag
+            File5Name
+            File5Photographer
+            File5Uri
+
+                        File6Alttext
+            File6Copyright
+            File6Description
+            file6Filetype
+            File6ID
+            File6Metatag
+            File6Name
+            File6Photographer
+            File6Uri
+
+            MaincatID
+            MaincatName
+
+            Channelname1
+            ChannelID1
+
+            Channelname2
+            ChannelID2
+
+            Channelname3
+            ChannelID3
+
+Channelname4
+            ChannelID4
+
+
+            Channelname5
+            ChannelID5
+
+
+            Channelname6
+            ChannelID6
+
+
+            Channelname7
+            ChannelID7
+
+
+            Channelname8
+            ChannelID8
+
+
+            Channelname9
+            ChannelID9
+
+
+            Channelname10
+            ChannelID10
+
+
+            Channelname11
+            ChannelID11
+
+
+            Channelname12
+            ChannelID12
+
+
+            Channelname13
+            ChannelID13
+
+
+            Channelname14
+            ChannelID14
+
+
+            Channelname15
+            ChannelID15
+
+
+            Channelname16
+            ChannelID16
+
+
+            Channelname17
+            ChannelID17
+
+
+            Channelname18
+            ChannelID18
+
+
+            Channelname19
+            ChannelID19
+
+
+
+
     }
 }
