f663b802586d5a6120e8112b3e201ebb16d72c80
Jonas Thorhauge Lykke <jona711p@student.eadania.dk>
2017-05-02 14:40:10 +0200

    Opening Hours done!

--- Foxtrot/Classes/OpeningHours.cs
+++ Foxtrot/Classes/OpeningHours.cs
@@ -12,7 +12,7 @@ namespace Classes
         public DateTime? StartDate { get; set; }
         public DateTime? EndDate { get; set; }
         public DateTime? StartTime { get; set; }
-        public DateTime? Endtime { get; set; }
+        public DateTime? EndTime { get; set; }
         public bool Monday { get; set; }
         public bool Tuesday { get; set; }
         public bool Wednesday { get; set; }
