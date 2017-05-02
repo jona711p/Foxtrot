423e15c4c6d16be0cb972cc3d8da960c7727c889
Jonas Thorhauge Lykke <jona711p@student.eadania.dk>
2017-05-02 10:46:08 +0200

    Blah

--- Foxtrot/Classes/CombiProduct.cs
+++ Foxtrot/Classes/CombiProduct.cs
@@ -6,7 +6,6 @@ namespace Classes
     {
         public int ID { get; set; }
         public DateTime CreationDate { get; set; }
-        public DateTime Period { get; set; }
         public float? PackagePrice { get; set; }
         public bool Availability { get; set; }
         public int Product_ID { get; set; }
