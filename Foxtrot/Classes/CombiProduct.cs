025f3584e50a7adb3d335ecab60e5a3a2473b851
Jonas Thorhauge Lykke <jona711p@student.eadania.dk>
2017-05-05 11:31:04 +0200

    Lidt flere rettelser

--- Foxtrot/Classes/CombiProduct.cs
+++ Foxtrot/Classes/CombiProduct.cs
@@ -6,10 +6,10 @@ namespace Classes
     {
         public int? ID { get; set; }
         public DateTime CreationDate { get; set; }
-        public float? PackagePrice { get; set; }
         public bool Availability { get; set; }
         public int? ProductID { get; set; }
         public int? ActorID { get; set; }
+        public float? PackagePrice { get; set; }
 
         public CombiProducts()
         {
--- Foxtrot/Classes/Product.cs
+++ Foxtrot/Classes/Product.cs
@@ -8,26 +8,35 @@ namespace Classes
         public int? ID { get; set; }
         public string Name { get; set; }
         public string Address { get; set; }
+        public City Cities { get; set; }
         public float? Latitude { get; set; }
         public float? Longitude { get; set; }
+
         public List<int?> ContactPhone { get; set; }
         public List<string> ContactEmail { get; set; }
         public List<int?> ContactFax { get; set; }
+
         public DateTime? CreationDate { get; set; }
         public List<OpeningHour> OpeningHours { get; set; }
-        public float? Price { get; set; }
+        public bool Availability { get; set; }
+
         public string Description { get; set; } //Text
         public List<ExtraDesription> ExtraDesription { get; set; }
-        public bool Availability { get; set; }
+
         public string Website { get; set; }
         public string CanonicalUrl { get; set; }
-        public List<File> Files { get; set; }
-        public City Cities { get; set; }
+
         public MainCategory MainCategories { get; set; }
         public Category Categories { get; set; }
+
+        public List<File> Files { get; set; }
+
         public int? ActorID { get; set; }
+
         public List<Event> Events { get; set; }
         public List<CombiProducts>  CombiProducts { get; set; }
+
+        public float? Price { get; set; }
 
         public Product()
         {
