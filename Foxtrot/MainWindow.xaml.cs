d1a5b1b51d5f80d9c159473a221d44aa117c3ac1
Thomas Nielsen <mini_hippo@hotmail.com>
2017-05-08 18:12:33 +0200

    Scrollbar + banner + ikon + fixes

--- Foxtrot/MainWindow.xaml.cs
+++ Foxtrot/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 using Classes;
 using Foxtrot.GUI;
 
@@ -11,26 +12,24 @@ namespace Foxtrot
     {
         public MainWindow()
         {
+            ResizeMode = ResizeMode.NoResize;
+
             XMLLogic.WatchXMLDir();
             InitializeComponent();
-            
-
-
             User tempUser = new User();
 
-
-           // Fills a list with actors and admins 
+            // Fills a list with actors and admins 
             tempUser.UserList = DBReadLogic.FillActorList(tempUser);
             tempUser.UserList = DBReadLogic.FillAdminList(tempUser);
             comboBox_Main_Usertype.ItemsSource = tempUser.UserList; //skal kun opdateres hvis 'button_User_Add_CreateUser' bliver trykket
 
-            textBox.Text = @"
-                            Skiveegnens Erhvervs - og Turistcenter Østerbro 7, 7800 Skive
-                            Tlf: +45 9614 7677 | info@skiveet.dk
-                            Åbningstider:
-                            Mandag – torsdag kl. 09.00 - 15.00
-                            Fredag kl. 09.00 - 14.00
-                            Turistinformation læs her";
+            //Smider tekst 'footer'
+            textBox.Text = @"Skiveegnens Erhvervs - og Turistcenter Østerbro 7, 7800 Skive
+Tlf: +45 9614 7677 | info@skiveet.dk
+Åbningstider:
+Mandag – torsdag kl. 09.00 - 15.00
+Fredag kl. 09.00 - 14.00
+Turistinformation læs her";
 
         }
         private void MenuItem_Menu_Frontpage_OnClick(object sender, RoutedEventArgs e)
