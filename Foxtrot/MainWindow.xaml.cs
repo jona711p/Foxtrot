92a00e2c5747527ed2d0583231886a4a43587496
Thomas Nielsen <mini_hippo@hotmail.com>
2017-05-08 12:58:06 +0200

    Skift mellem menuer

--- Foxtrot/MainWindow.xaml.cs
+++ Foxtrot/MainWindow.xaml.cs
@@ -13,39 +13,14 @@ namespace Foxtrot
             XMLLogic.WatchXMLDir();
             InitializeComponent();
         }
-
-
-        //private void MenuItem_OnClick(object sender, RoutedEventArgs e)
-        //{
-        //    string tempString;
-        //    sender.Header = tempString;
-        //    //switch (sender.Header)
-        //    //{
-        //    //    case "Opret":
-        //    //        MessageBox.Show("Opret");
-        //    //        break;
-        //    //    case "Rediger":
-        //    //        MessageBox.Show("Rediger");
-        //    //        break;
-        //    //    case "Slet":
-        //    //        MessageBox.Show("Slet");
-        //    //        break;
-        //    //}
-        //    //MessageBox.Show("hest");
-        //    //MainFrame.Source = new Uri("GUI/Users_Add.xaml", UriKind.Relative); // initialize frame with the "test1" view
-
-        //    //Page opret = new Page();
-        //    //frame.Content = Users_Add;
-        //}
-
         private void MenuItem_Menu_Frontpage_OnClick(object sender, RoutedEventArgs e)
         {
-            throw new NotImplementedException();
+            MessageBox.Show("FORSIDE!");
         }
 
         private void MenuItem_User_Create_OnClick(object sender, RoutedEventArgs e)
         {
-            throw new NotImplementedException();
+            MainFrame.Source = new Uri("GUI/User/User_Add.xaml", UriKind.Relative); // initialize frame with the "test1" view        
         }
 
         private void MenuItem_User_Edit_OnClick(object sender, RoutedEventArgs e)
@@ -55,7 +30,7 @@ namespace Foxtrot
 
         private void MenuItem_Product_Create_OnClick(object sender, RoutedEventArgs e)
         {
-            throw new NotImplementedException();
+            MainFrame.Source = new Uri("GUI/Product/Product_Add.xaml", UriKind.Relative); // initialize frame with the "test1" view        }
         }
 
         private void MenuItem_Product_Edit_OnClick(object sender, RoutedEventArgs e)
