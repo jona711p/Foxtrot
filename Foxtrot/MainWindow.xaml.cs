c05e44704859d3bb5d6d2074a68740cacd1ddb0b
Thomas Nielsen <mini_hippo@hotmail.com>
2017-05-09 12:53:08 +0200

    æ

--- Foxtrot/MainWindow.xaml.cs
+++ Foxtrot/MainWindow.xaml.cs
@@ -59,6 +59,7 @@ Fredag kl. 09.00 - 14.00";
         {
             MainFrame.Content = new Product_Add();
             DBShowProducts.FillTable(temProduct);
+           //dataGrid_Product_List
         }
 
         private void MenuItem_Product_Edit_Delete_OnClick(object sender, RoutedEventArgs e)
