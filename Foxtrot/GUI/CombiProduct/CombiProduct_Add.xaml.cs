d51907a77222b44187b82a1c6bb3269add72b843
Paask <Paask@MIKAEL>
2017-05-16 11:20:42 +0200

    .

---
+++ Foxtrot/GUI/CombiProduct/CombiProduct_Add.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Foxtrot.GUI.CombiProduct
+{
+    /// <summary>
+    /// Interaction logic for CombiProduct_Add.xaml
+    /// </summary>
+    public partial class CombiProduct_Add : Page
+    {
+        public CombiProduct_Add()
+        {
+            InitializeComponent();
+        }
+    }
+}
