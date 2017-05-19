23fcec1420227f8013c01a38f74374b19cbea9f8
mini_ <mini_@DESKTOP-6CMBIKB>
2017-05-19 13:35:08 +0200

    sg

--- Foxtrot/Classes/CombiProduct.cs
+++ Foxtrot/Classes/CombiProduct.cs
@@ -1,15 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Foxtrot.Classes
 {
     public class CombiProduct
     {
-        public int? ID { get; set; }
-        public DateTime CreationDate { get; set; }
+        public int? ID { get; set; }//auto
+        public DateTime CreationDate { get; set; }//auto
+        public string Name { get; set; }
         public bool Availability { get; set; }
-        public int? ProductID { get; set; }
-        public int? UserID { get; set; }
-        public float? PackagePrice { get; set; }
+        public List<int?> ProductID { get; set; }//list
+        public int? UserID { get; set; }//auto
+        public float? PackagePrice { get; set; } //auto
 
         public CombiProduct()
         {
