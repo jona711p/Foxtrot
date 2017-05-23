9a80d56fe0c39f734229f721be10ab8d7486f44a
Paask <Paask@MIKAEL>
2017-05-23 11:56:17 +0200

    .

--- Foxtrot/Classes/DB/DBReadLogic.cs
+++ Foxtrot/Classes/DB/DBReadLogic.cs
@@ -589,8 +589,7 @@ namespace Foxtrot.Classes.DB
 
                 SqlDataReader reader = command.ExecuteReader();
                 reader.Read();
-                inputCombiProductProduct.ID = int.Parse(reader["ID"].ToString());
-                inputCombiProductProduct.CreationDate = DateTime.Parse(reader["CreationDate"].ToString());
+                inputCombiProductProduct.ID = int.Parse(reader["CombiProductID"].ToString());
                 inputCombiProductProduct.PackagePrice = DBSortingLogic.ConvertToNullableFloat(reader["PackagePrice"].ToString());
                 inputCombiProductProduct.Availability = bool.Parse(reader["Availability"].ToString());
                 inputCombiProductProduct.Name = reader["Name"].ToString();
